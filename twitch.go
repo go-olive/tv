2f1cab8141cba9edce9274e3ee063d3c5e8a6db5
luxcgo <luxcgo@gmail.com>
2022-04-25 00:33:30 +0800

    twitch: new site

---
+++ twitch.go
@@ -0,0 +1,52 @@
+package tv
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/go-olive/tv/util"
+)
+
+func init() {
+	registerSite("twitch", &twitch{})
+}
+
+type twitch struct {
+	base
+}
+
+func (this *twitch) Name() string {
+	return "推趣"
+}
+
+func (this *twitch) Snap(tv *Tv) error {
+	tv.Info = &Info{
+		Timestamp: time.Now().Unix(),
+	}
+	return this.set(tv)
+}
+
+func (this *twitch) set(tv *Tv) error {
+	roomUrl := fmt.Sprintf("https://www.twitch.tv/%s", tv.RoomID)
+	content, err := util.GetURLContent(roomUrl)
+	if err != nil {
+		return err
+	}
+
+	tv.roomOn = strings.Contains(content, `"isLiveBroadcast":true`)
+	if !tv.roomOn {
+		return nil
+	}
+
+	tv.streamUrl = roomUrl
+	title, err := util.Match(`<meta name="description" content="([^"]+)"`, content)
+	if err != nil {
+		return nil
+	}
+
+	tv.roomName = title
+	tv.roomNameSet = true
+
+	return nil
+}
